d5160237c2585ebed37ff827814678f7843ad1c7
Harikrishnan N <harikrishnansr92@live.com>
2016-09-24 21:56:17 +0530

    Decrypt ceaser cipher

--- ceaser.c
+++ ceaser.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
-char string[] = "asz\n";
-
 int main(){
-	char plain_text[100];
+	char plain_text[100],string[100];
 	int y=0,z,i=0;
 	printf("Enter the string\n");
 	scanf("%s",string);
 	printf("Enter shift value\n");
 	scanf("%d",&z);
-	while(string[i] != '\n'){
-		plain_text[i] = (char)(string[i]-z);			
-		i++;
+	while(string[i] != '\0'){
+		if(string[i] >= 'a' && string[i] <= 'z'){
+			plain_text[i] = (char)(((string[i] - z - 'a' + 26)%26) + 'a');
+		}
+		else{
+			plain_text[i] = (char)(string[i] - z);
+		}
+		i++;	
 	}
 	printf("%s\n",plain_text);	
 	return 0;
